2b4d4fb09c709077efc98301aa652a6482d6a9ea
SamuelWango <wangos478@gmail.com>
2023-07-07 11:08:40 +0300

    troubleshooting

--- script.js
+++ script.js
@@ -1,57 +1,64 @@
-    // // Fetch the JSON file
-    // fetch('diary.json')
-    //   .then(response => response.json()) // Parse the response as JSON
-    //   .then(data => {
-    //     // Access the diary entries
-    //     const diaryEntries = data.diaryEntries;
-
-    //     // Get the 'entries' element
-    //     const entriesContainer = document.getElementById('diary');
-
-    //     // Iterate through the diary entries
-    //     diaryEntries.forEach(entry => {
-    //       // Create a new paragraph element for each entry
-    //       const entryElement = document.createElement('p');
-    //       entryElement.textContent = `${entry.date}: ${entry.entry}`;
-
-    //       // Append the entry element to the 'entries' container
-    //       entriesContainer.appendChild(entryElement);
-    //     });
-    //   })
-    //   .catch(error => {
-    //     console.log('An error occurred:', error);
-    //   });
-// Function to fetch the quotes from the JSON file
-function fetchQuotes() {
+// fetch the quote from the JSON file
+function fetchQuote() {
   return fetch('quotes.json')
     .then(response => response.json())
-    .then(data => data.quotes)
+    .then(data => data)
     .catch(error => {
-      console.log('An error occurred while fetching the quotes:', error);
-      return [];
+      console.log('An error occurred while fetching the quote:', error);
+      return null;
     });
 }
 
-// Function to show a random quote
-function showRandomQuote() {
-  fetchQuotes()
-    .then(quotes => {
-      if (quotes.length > 0) {
-        // Get a random index from the quotes array
-        const randomIndex = Math.floor(Math.random() * quotes.length);
-
-        // Get the random quote object
-        const randomQuote = quotes[randomIndex];
-
-        // Alert the quote with the author
-        alert(`${randomQuote.quote}\n- ${randomQuote.author}`);
+// display the quote
+function showQuote() {
+  fetchQuote()
+    .then(quote => {
+      if (quote) {
+        const { quote: motivationalQuote, author } = quote;
+        alert(`Motivational Quote:\n\n"${motivationalQuote}" - ${author}`);
       } else {
-        alert('No quotes available.');
+        alert('No quote available.');
       }
     });
 }
 
-// Attach click event to the button
-document.getElementById("start").addEventListener("click", showRandomQuote);
+//  click event to the button
+document.getElementById("start").addEventListener("click", showQuote);
 
-      
+// delete an entry by clearing the input field
+function deleteEntry(inputId) {
+  const inputField = document.getElementById(inputId);
+  inputField.value = '';
+}
+
+
+
+// save an entry to the JSON file
+function saveEntry(inputId) {
+  const inputField = document.getElementById(inputId);
+  const entryValue = inputField.value;
+  const date = new Date().toLocaleDateString();
+  const day = inputId.replace('Input', '');
+
+  // object with the entry data
+  const entryData = {
+    day: day,
+    date: date,
+    entry: entryValue
+  };
+
+  // Get the existing entries from the JSON file or create an empty array
+  let entries = localStorage.getItem('entries');
+  entries = entries ? JSON.parse(entries) : [];
+
+  // Add the new entry to the array
+  entries.push(entryData);
+
+  // Save the updated entries to the JSON file
+  localStorage.setItem('entries', JSON.stringify(entries));
+
+  // Clear the input field
+  inputField.value = '';
+  alert('Entry saved successfully!');
+}
+
